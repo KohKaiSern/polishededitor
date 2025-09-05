b6f20141b33cacb911dcd6296e3d7f4e00c0648d
Rev3lation <kaisern.koh@gmail.com>
2025-09-05 11:47:57 +0800

    Implements items.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import getAbilities from "./abilities.js";
 import getMoves from "./moves.js";
+import getItems from "./items.js";
 const app = express();
 
 //Middleware
@@ -15,6 +16,9 @@ app.get("/abilities", (req, res) => res.json(getAbilities()));
 
 //Moves Route
 app.get("/moves", (req, res) => res.json(getMoves()));
+
+//Items Route
+app.get("/items", (req, res) => res.json(getItems()));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
---
+++ backend/api/items.js
@@ -0,0 +1,97 @@
+import { readFile } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import splitFile from "../lib/split.js";
+import reduce from "../lib/reduce.js";
+
+let items = {
+  Polished: [],
+  Faithful: [],
+};
+
+//Paths
+const namesASM = join(
+  __dirname,
+  "../public/polishedcrystal/data/items/names.asm"
+);
+const descriptionsASM = join(
+  __dirname,
+  "../public/polishedcrystal/data/items/descriptions.asm"
+);
+
+const extractNames = (data, PF) => {
+  data = data.filter((line) => line.startsWith("li "));
+  data = data.map((line) => line.slice(0, -1).split('"').at(-1));
+  for (let item of data.slice(1)) {
+    items[PF].push({
+      ID: reduce(item),
+      Name: item,
+      "Item Number": data.indexOf(item),
+    });
+  }
+};
+
+const extractDescriptions = (data, PF) => {
+  for (let i = 0; i < data.length; i++) {
+    let itemsArr = [];
+    let description = "";
+    //Once we hit a line that has signals a description
+    while (data[i].endsWith("Desc:")) {
+      //We add in every item that the description applies to
+      itemsArr.push(reduce(data[i].replace("Desc:", "")));
+      i++;
+    }
+    //Now we actually grab the description
+    while (itemsArr.length > 0 && data[i] != "done") {
+      description += data[i].slice(0, -1).split('"').at(-1);
+      if (description.at(-1) === "-") {
+        description = description.slice(0, -1);
+        i++;
+        continue;
+      }
+      description += " ";
+      i++;
+    }
+    //Now we add it to items
+    for (let entry of itemsArr) {
+      let item = items[PF].find((item) => item["ID"] === entry);
+      if (item) {
+        item["Description"] = description.trim();
+      }
+    }
+  }
+};
+
+const coverInconsistencies = (PF) => {
+  let altPF = PF === "Polished" ? "Faithful" : "Polished";
+  for (let item of items[PF]) {
+    if (!item.hasOwnProperty("Description")) {
+      item["Description"] =
+        items[altPF][item["Item Number"] - 1]["Description"];
+    }
+  }
+};
+
+//#1: Names
+let raw = await readFile(namesASM, "utf-8");
+const namesFILES = splitFile(raw);
+extractNames(namesFILES[0], "Polished");
+extractNames(namesFILES[1], "Faithful");
+
+//#2: Descriptions
+raw = await readFile(descriptionsASM, "utf-8");
+const descriptionsFILES = splitFile(raw);
+extractDescriptions(descriptionsFILES[0], "Polished");
+extractDescriptions(descriptionsFILES[1], "Faithful");
+
+coverInconsistencies("Polished");
+coverInconsistencies("Faithful");
+
+//Items GETTER
+const getItems = () => {
+  return items;
+};
+
+export default getItems;
