a802a8527accaa17b1e52101ee980563b5fb40fc
Rev3lation <kaisern.koh@gmail.com>
2025-09-05 10:39:57 +0800

    Check if it's a routing issue

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.get("/", (req, res) => res.send("Polished Editor Backend Functional!"));
 
-app.get("/api/abilities", (req, res) => res.send("Testing Route"));
+app.get("/abilities", (req, res) => res.send("Testing Route"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
