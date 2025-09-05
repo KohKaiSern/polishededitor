c98664875b0f6bc1d34994dca1830243532da2df
Rev3lation <kaisern.koh@gmail.com>
2025-09-05 10:36:31 +0800

    Check if the issue is .send() vs .json()

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.get("/", (req, res) => res.send("Polished Editor Backend Functional!"));
 
-app.get("/api/abilities", (req, res) => res.json(getAbilities()))
+app.get("/api/abilities", (req, res) => res.send(JSON.stringify(getAbilities())));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
