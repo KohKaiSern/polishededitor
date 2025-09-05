033c1fd54a1e3873a8919fbb8c10de18a0bcdc13
Rev3lation <kaisern.koh@gmail.com>
2025-09-05 10:45:33 +0800

    Introduces lib, preparing for submodule addition

--- backend/api/abilities.js
+++ backend/api/abilities.js
@@ -1,4 +1,6 @@
 import { readFile } from "fs/promises";
+import splitFile from "../lib/split.js";
+import reduce from "../lib/reduce.js";
 
 let abilities = {
   Polished: [],
--- backend/api/index.js
+++ backend/api/index.js
@@ -3,10 +3,13 @@ import cors from "cors";
 import getAbilities from "./abilities.js";
 const app = express();
 
+//Middleware
 app.use(cors());
 
+//Home Route
 app.get("/", (req, res) => res.send("Polished Editor Backend Functional!"));
 
+//Abilities Route
 app.get("/abilities", (req, res) => res.json(getAbilities()));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
