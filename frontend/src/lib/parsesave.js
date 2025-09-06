a8369980fdb062cc6c1d0220380fe1e69d587ffa
Rev3lation <kaisern.koh@gmail.com>
2025-09-06 17:05:53 +0800

    Documentational change

--- frontend/src/lib/parsesave.js
+++ frontend/src/lib/parsesave.js
@@ -3,8 +3,6 @@ import parseMon from "./parsemon.js";
 import getAddresses from "./addresses.js";
 
 //Determines the memory address of each Pokemon and calls the parse function on it.
-//Details of the NewBox system can be found at:
-//https://github.com/Rangi42/polishedcrystal/blob/master/docs/newbox_format.md
 
 export const parseSave = (save, PF) => {
   //Get 20x20 Array of Addresses
