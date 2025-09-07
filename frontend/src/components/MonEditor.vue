5e6cad9f7bc70b338337062c48b9b071720f81ed
Rev3lation <kaisern.koh@gmail.com>
2025-09-07 21:25:49 +0800

    Refactor Pt 7

--- frontend/src/components/MonEditor.vue
+++ frontend/src/components/MonEditor.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { watch } from "vue";
 import Accordion from "primevue/accordion";
 import AccordionPanel from "primevue/accordionpanel";
 import AccordionHeader from "primevue/accordionheader";
@@ -13,16 +12,6 @@ const pokemon = defineModel();
 </script>
 
 <template>
-  <div class="flex flex-col gap-3">
-    <IftaLabel>
-      <InputText id="nickname" />
-      <label for="nickname">Nickname</label>
-    </IftaLabel>
-    <IftaLabel>
-      <InputNumber id="level" v-model="pokemon['Level']" showButtons :min="1" :max="100" fluid/>
-      <label for="level">Level</label>
-    </IftaLabel>
-  </div>
   <Accordion class="m-0">
     <AccordionPanel value="stats">
       <AccordionHeader>DVs, EVs & Natures</AccordionHeader>
@@ -97,34 +86,6 @@ const pokemon = defineModel();
             <label for="spd">Sp. Def</label>
           </IftaLabel>
         </div>
-      </AccordionContent>
-    </AccordionPanel>
-    <AccordionPanel value="1">
-      <AccordionHeader>Header II</AccordionHeader>
-      <AccordionContent>
-        <p class="m-0">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit,
-          sed quia non numquam eius modi.
-        </p>
-      </AccordionContent>
-    </AccordionPanel>
-    <AccordionPanel value="2">
-      <AccordionHeader>Header III</AccordionHeader>
-      <AccordionContent>
-        <p class="m-0">
-          At vero eos et accusamus et iusto odio dignissimos ducimus qui
-          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati cupiditate non provident,
-          similique sunt in culpa qui officia deserunt mollitia animi, id est
-          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-          distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-          cumque nihil impedit quo minus.
-        </p>
       </AccordionContent>
     </AccordionPanel>
   </Accordion>
