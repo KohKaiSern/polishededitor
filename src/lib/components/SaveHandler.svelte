ec9bcddb25ba58f27d745839c008b2e6e36cc487
Rev3lation <kaisern.koh@gmail.com>
2025-09-20 16:16:35 +0800

    Begin Save File Handling

---
+++ src/lib/components/SaveHandler.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Heading, P, Label, Fileupload, Button } from 'flowbite-svelte';
+</script>
+
+<header>
+	<Heading tag="h1" class="mb-5">Polished Editor</Heading>
+	<Label class="mb-3">Upload Save</Label>
+	<div class="flex gap-3">
+		<Fileupload />
+		<Button color="green" class="whitespace-nowrap">Download Save</Button>
+	</div>
+</header>
