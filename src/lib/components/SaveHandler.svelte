34ab24fe1d0412e37495705bf4e77b1ddbf7ff48
Rev3lation <kaisern.koh@gmail.com>
2025-09-21 16:46:39 +0800

    Added Helper Text

--- src/lib/components/SaveHandler.svelte
+++ src/lib/components/SaveHandler.svelte
@@ -1,12 +1,21 @@
 <script>
-	import { Heading, P, Label, Fileupload, Button } from 'flowbite-svelte';
+	import { Heading, P, Label, Fileupload, Button, Helper } from 'flowbite-svelte';
+	
+	let file = $state(null);
+
+	const handleSave = () => {
+		return
+	}
+
+	$inspect(file)
 </script>
 
 <header>
 	<Heading tag="h1" class="mb-5">Polished Editor</Heading>
-	<Label class="mb-3">Upload Save</Label>
-	<div class="flex gap-3">
-		<Fileupload />
+	<Label class="mb-2">Upload Save</Label>
+	<div class="flex gap-3 mb-2">
+		<Fileupload bind:files={file} onchange={handleSave}/>
 		<Button color="green" class="whitespace-nowrap">Download Save</Button>
 	</div>
+	<Helper>.SAV or .SRM (Max 33KB).</Helper>
 </header>
