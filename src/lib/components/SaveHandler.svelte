5a9d49b43d42bb99d20048e7df3da87bcbeb53a2
Koh Kai Sern <kaisern.koh@gmail.com>
2025-09-29 23:19:08 +0800

    Added documentation to SaveHandler function

--- src/lib/components/SaveHandler.svelte
+++ src/lib/components/SaveHandler.svelte
@@ -1,20 +1,24 @@
 <script>
 	import { Heading, P, Label, Fileupload, Button, Helper } from 'flowbite-svelte';
-	
+
 	let file = $state(null);
+	// This function should:
+	// #1: Verify the size of the file
+	// #2: Parse and verify the save number of the file
+	// #3: Run the mon parse
 
 	const handleSave = () => {
-		return
-	}
+		return;
+	};
 
-	$inspect(file)
+	$inspect(file);
 </script>
 
 <header>
 	<Heading tag="h1" class="mb-5">Polished Editor</Heading>
 	<Label class="mb-2">Upload Save</Label>
-	<div class="flex gap-3 mb-2">
-		<Fileupload bind:files={file} onchange={handleSave}/>
+	<div class="mb-2 flex gap-3">
+		<Fileupload bind:files={file} onchange={handleSave} />
 		<Button color="green" class="whitespace-nowrap">Download Save</Button>
 	</div>
 	<Helper>.SAV or .SRM (Max 33KB).</Helper>
