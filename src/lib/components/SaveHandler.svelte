5a086df2e2e0090fd95c4e7160874f06b524c2cb
Koh Kai Sern <kaisern.koh@gmail.com>
2025-09-30 20:21:01 +0800

    Second try

--- src/lib/components/SaveHandler.svelte
+++ src/lib/components/SaveHandler.svelte
@@ -49,7 +49,7 @@
 <header>
 	<div class="mb-5 flex flex-wrap items-start justify-between gap-5">
 		<Heading tag="h1">Polished Editor</Heading>
-		<Toggle color="purple" bind:checked disabled={file === null ? false : true}
+		<Toggle color="purple" bind:checked disabled={!!file}
 			>{PF.at(0).toUpperCase() + PF.slice(1)}</Toggle
 		>
 	</div>
