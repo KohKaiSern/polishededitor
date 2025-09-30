df1f913ad824831cab8b9604f96e15e92045e350
Koh Kai Sern <kaisern.koh@gmail.com>
2025-09-30 15:01:20 +0800

    Adds save validation toast

--- src/lib/components/SaveHandler.svelte
+++ src/lib/components/SaveHandler.svelte
@@ -1,23 +1,22 @@
 <script>
 	import { Heading, P, Label, Fileupload, Button, Helper, Toast } from 'flowbite-svelte';
-	import { buf2hex } from './SaveHandler.svelte.js';
+	import { buf2hex, checkSaveVersion } from './SaveHandler.svelte.js';
 
 	let file = $state(null);
 	let toastMsg = $state(null);
-	let fileHex = $state(null);
+
 	// This function should:
 	// #1: Verify the size of the file
 	// #2: Parse and verify the save number of the file
 	// #3: Run the mon parse
-
 	const handleSave = async () => {
 		if (file[0].size > 33000) {
 			toastMsg =
 				"This doesn't seem like a save file. Make sure it's a battery save and not a save state.";
 			return;
 		}
-		toastMsg = 'File Verified!';
-		fileHex = buf2hex(await file[0].arrayBuffer());
+		const fileHex = buf2hex(await file[0].arrayBuffer());
+		toastMsg = checkSaveVersion(fileHex);
 		return;
 	};
 </script>
--- src/lib/components/SaveHandler.svelte.js
+++ src/lib/components/SaveHandler.svelte.js
@@ -1,4 +1,15 @@
+import versions from '$data/versions.json'
+import addresses from '$data/addresses.json'
+
 export const buf2hex = (buffer) => {
   return Array.from([...new Uint8Array(buffer)]
       .map(x => x.toString(16).padStart(2, '0').toUpperCase()))
+}
+
+export const checkSaveVersion = (fileHex) => {
+  const saveVersion = parseInt(fileHex[parseInt(addresses.sSaveVersion, 16) + 1], 16)
+  if (saveVersion === parseInt(versions.save)) {
+    return "Save Validated!"
+  }
+  return `This save file has version ${saveVersion}. The current save version is ${versions.save}. Ensure that your game is on version ${versions.game}.`
 }
