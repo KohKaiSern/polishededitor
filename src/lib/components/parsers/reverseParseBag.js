45a7e4c5cd5134053c98103cf8499eb7448d9e8e
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-01 18:46:05 +0800

    RParse Bag Done

--- src/lib/components/parsers/reverseParseBag.js
+++ src/lib/components/parsers/reverseParseBag.js
@@ -1,5 +1,32 @@
-export const reverseParseBag = (fileHex, items, PF) => {
+import addresses from '$data/addresses.json'
+import items from '$data/items.json'
+
+export const reverseParseBag = (fileHex, bag, PF) => {
+
+  const address = parseInt(addresses.sBackupPlayerData, 16) + 943;
+
+  for (let i = 0; i < bag.items.count; i++) {
+    const itemNo = items[PF].find((item) => item.name === bag.items.contents[i].name).itemNo.toString(16);
+    fileHex[address + (2 * i + 1)] = itemNo
+    fileHex[address + 2 * (i + 1)] = bag.items.contents[i].qty.toString(16)
+  }
+  for (let i = 0; i < bag.meds.count; i++) {
+    const itemNo = items[PF].find((item) => item.name === bag.meds.contents[i].name).itemNo.toString(16);
+    fileHex[address + (2 * i + 1)] = itemNo
+    fileHex[address + 2 * (i + 1)] = bag.meds.contents[i].qty.toString(16)
+  }
+  for (let i = 0; i < bag.balls.count; i++) {
+    const itemNo = items[PF].find((item) => item.name === bag.balls.contents[i].name).itemNo.toString(16);
+    fileHex[address + (2 * i + 1)] = itemNo
+    fileHex[address + 2 * (i + 1)] = bag.balls.contents[i].qty.toString(16)
+  }
+  for (let i = 0; i < bag.berries.count; i++) {
+    const itemNo = items[PF].find((item) => item.name === bag.berries.contents[i].name).itemNo.toString(16);
+    fileHex[address + (2 * i + 1)] = itemNo
+    fileHex[address + 2 * (i + 1)] = bag.berries.contents[i].qty.toString(16)
+  }
+
 	return fileHex;
-};
+}
 
 export default reverseParseBag;
