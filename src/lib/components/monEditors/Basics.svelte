070b19e0aad9756d01b7124cf59a46c175dc92a3
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-02 19:39:58 +0800

    Feature Parity!

--- src/lib/components/monEditors/Basics.svelte
+++ src/lib/components/monEditors/Basics.svelte
@@ -23,9 +23,43 @@
 			.forms.find((form) => form.name === mon.form)
 			.growthRate.slice(6);
 	};
+
+	const resetSpecies = () => {
+		const species = pokemon[PF].find((pokemon) => pokemon.name === mon.species);
+		const form = species.forms[0];
+		mon.form = form.name;
+		mon.ability = form.abilities[0];
+		setExpForLvl();
+	};
+
+	const resetForm = () => {
+		const form = pokemon[PF].find((pokemon) => pokemon.name === mon.species).forms.find(
+			(form) => form.name === mon.form
+		);
+		mon.ability = form.abilities[0];
+		setExpForLvl();
+	};
 </script>
 
-<Heading tag="h6" class="mb-3">Held Item</Heading>
+<Heading tag="h6" class="mb-3">Species & Form</Heading>
+<div class="flex gap-3">
+	<DropdownSearch
+		class="flex-1"
+		bind:value={mon.species}
+		options={pokemon[PF].map((pokemon) => pokemon.name)}
+		onchange={resetSpecies}
+	/>
+	<DropdownSearch
+		class="flex-1"
+		bind:value={mon.form}
+		options={pokemon[PF].find((pokemon) => pokemon.name === mon.species).forms.map(
+			(form) => form.name
+		)}
+		onchange={resetForm}
+	/>
+</div>
+
+<Heading tag="h6" class="mt-3 mb-3">Held Item</Heading>
 <DropdownSearch
 	bind:value={mon.heldItem}
 	options={['None', ...items[PF].map((item) => item.name)]}
