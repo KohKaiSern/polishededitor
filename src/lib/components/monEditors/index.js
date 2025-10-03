5d14bc5d170e153e71a512780d0dfe0359449a82
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-03 10:36:24 +0800

    Add index.js files

---
+++ src/lib/components/monEditors/index.js
@@ -0,0 +1,4 @@
+export { default as Basics } from './Basics.svelte'
+export { default as DVEVNature } from './DVEVNature.svelte'
+export { default as Misc } from './Misc.svelte'
+export { default as Moveset } from './Moveset.svelte'
