6a097cdc1dea88f47e9f98db58c3e95b8379a379
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-03 10:25:10 +0800

    Abstracted Bag Sections

--- src/lib/components/BagEditor.svelte
+++ src/lib/components/BagEditor.svelte
@@ -1,16 +1,16 @@
 <script>
-	import Items from './bagEditors/Items.svelte';
-	import Meds from './bagEditors/Meds.svelte';
-	import Balls from './bagEditors/Balls.svelte';
-	import Berries from './bagEditors/Berries.svelte';
+	import BagSection from './bagEditors/BagSection.svelte';
 	import { RadioSelect } from '$components/UI';
 	let { bag = $bindable(), PF } = $props();
 	let selectedSlot = $state('Items');
+	const slots = [
+		{ type: 'item', label: 'Item', key: 'items', size: 75, option: 'Items' },
+		{ type: 'medicine', label: 'Medicine', key: 'meds', size: 37, option: 'Medicines' },
+		{ type: 'ball', label: 'Ball', key: 'balls', size: 25, option: 'Balls' },
+		{ type: 'berries', label: 'Berry', key: 'berries', size: 31, option: 'Berries' }
+	];
 </script>
 
-<RadioSelect bind:value={selectedSlot} options={['Items', 'Medicines', 'Balls', 'Berries']} />
+<RadioSelect bind:value={selectedSlot} options={slots.map((slot) => slot.option)} />
 
-{#if selectedSlot === 'Items'}<Items bind:bag {PF} />{/if}
-{#if selectedSlot === 'Medicines'}<Meds bind:bag {PF} />{/if}
-{#if selectedSlot === 'Balls'}<Balls bind:bag {PF} />{/if}
-{#if selectedSlot === 'Berries'}<Berries bind:bag {PF} />{/if}
+<BagSection bind:bag {PF} {...slots.find((slot) => slot.option === selectedSlot)} />
--- src/lib/components/bagEditors/BagSection.svelte
+++ src/lib/components/bagEditors/BagSection.svelte
@@ -3,45 +3,53 @@
 	import { PlusOutline, TrashBinSolid } from 'flowbite-svelte-icons';
 	import { DropdownSearch, NumberInput } from '$components/UI';
 	import items from '$data/items.json';
-	let { bag = $bindable(), PF } = $props();
+
+	let { bag = $bindable(), PF, type, label, key, size } = $props();
+
 	const addItem = (i) => {
 		for (let item of items[PF]) {
-			if (item.type === 'berries') {
-				bag.berries.contents[i].name = item.name;
+			if (item.type === type) {
+				bag[key].contents[i].name = item.name;
 				break;
 			}
 		}
-		bag.berries.contents[i].qty = 1;
-		bag.berries.count += 1;
+		bag[key].contents[i].qty = 1;
+		bag[key].count += 1;
 	};
+
 	const deleteItem = (i) => {
-		bag.berries.contents = bag.berries.contents
+		bag[key].contents = bag[key].contents
 			.slice(0, i)
-			.concat(bag.berries.contents.slice(i + 1))
+			.concat(bag[key].contents.slice(i + 1))
 			.concat({ name: null, qty: null });
-		bag.berries.count -= 1;
+		bag[key].count -= 1;
 	};
 </script>
 
 <Listgroup class="mt-5">
-	<ListgroupItem class="flex justify-between italic"><P>Berry</P><P>Quantity</P></ListgroupItem>
-	{#each Array(31) as _, i}
-		{#if !!bag.berries.contents[i].name}
+	<ListgroupItem class="flex justify-between italic">
+		<P>{label}</P>
+		<P>Quantity</P>
+	</ListgroupItem>
+
+	{#each Array(size) as _, i}
+		{#if !!bag[key].contents[i].name}
 			<ListgroupItem class="flex w-full justify-between">
 				<div class="flex flex-col gap-3 pt-3">
 					<DropdownSearch
-						bind:value={bag.berries.contents[i].name}
-						options={items[PF].filter((item) => item.type === 'berries').map((item) => item.name)}
+						bind:value={bag[key].contents[i].name}
+						options={items[PF].filter((item) => item.type === type).map((item) => item.name)}
 					/>
 					<P class="mb-1 w-full italic">
-						{items[PF].find((item) => item.name === bag.berries.contents[i].name).description}
+						{items[PF].find((item) => item.name === bag[key].contents[i].name).description}
 					</P>
 				</div>
+
 				<div class="flex flex-wrap gap-2">
-					<Button class="p-2!" color="red" outline onclick={() => deleteItem(i)}
-						><TrashBinSolid class="h-6 w-6" /></Button
-					>
-					<NumberInput bind:value={bag.berries.contents[i].qty} min={1} max={99} />
+					<Button class="p-2!" color="red" outline onclick={() => deleteItem(i)}>
+						<TrashBinSolid class="h-6 w-6" />
+					</Button>
+					<NumberInput bind:value={bag[key].contents[i].qty} min={1} max={99} />
 				</div>
 			</ListgroupItem>
 		{:else}
