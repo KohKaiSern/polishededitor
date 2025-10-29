96583a03eb78788a77b8b3687f5ab8082f8f705e
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-29 15:27:47 +0800

    Delete old extractors

--- src/extractors/index.ts
+++ src/extractors/index.ts
@@ -1,38 +1,7 @@
-import abilities from './abilities';
-import addresses from './addresses';
-import apricorns from './apricorns';
-import badges from './badges';
-import boxThemes from './boxThemes';
-import charmap from './charmap';
-import expCandy from './expCandy';
-import growthRateCoefficients from './growthRateCoefficients';
-import items from './items';
-import keyItems from './keyItems';
-import locations from './locations';
-import moves from './moves';
-import pokemon from './pokemon';
-import tmhm from './tmhm';
 import { writeJSON } from './utils';
-import versions from './versions';
-import wings from './wings';
+
 
 for (const [name, obj] of Object.entries({
-	addresses,
-	locations,
-	abilities,
-	items,
-	keyItems,
-	moves,
-	boxThemes,
-	growthRateCoefficients,
-	versions,
-	badges,
-	charmap,
-	wings,
-	expCandy,
-	pokemon,
-	apricorns,
-	tmhm
 })) {
-	writeJSON(name, obj);
+  writeJSON(name, obj);
 }
