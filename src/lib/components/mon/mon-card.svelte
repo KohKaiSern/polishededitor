3311f79606a529800119d2a4a4cf3266106f779c
Koh Kai Sern <kaisern.koh@gmail.com>
2025-12-31 04:08:17 +0800

    Mon CarD

--- src/lib/components/mon/mon-card.svelte
+++ src/lib/components/mon/mon-card.svelte
@@ -9,56 +9,85 @@
 	import pokemon from '$data/pokemon.json';
 	import ItemIcon from '$ui/item-icon.svelte';
 	import Heal from '$ui/heal.svelte';
-
 	interface MonCardProps {
 		mon: PartyMon | BoxMon;
 		PF: 'polished' | 'faithful';
 		ondelete: () => void;
 	}
-
 	let { mon = $bindable(), PF, ondelete }: MonCardProps = $props();
-
 	let species = $derived(pokemon[PF].find((p) => p.name === mon.species)!);
 	let form = $derived(species.forms.find((f) => f.name === mon.form)!);
-
+	let borderColors = $derived.by(() => {
+		return form.types.map(
+			(t) =>
+				({
+					BUG: '#92BC2C',
+					DARK: '#595761',
+					DRAGON: '#0C69C8',
+					ELECTRIC: '#F2D94E',
+					FIRE: '#FBA54C',
+					FAIRY: '#EE90E6',
+					FIGHTING: '#D3425F',
+					FLYING: '#A1BBEC',
+					GHOST: '#5F6DBC',
+					GRASS: '#5FBD58',
+					GROUND: '#DA7C4D',
+					ICE: '#75D0C1',
+					NORMAL: '#A0A29F',
+					POISON: '#B763CF',
+					PSYCHIC: '#FA8581',
+					ROCK: '#C9BB8A',
+					STEEL: '#5695A3',
+					WATER: '#539DDF'
+				})[t]!
+		);
+	});
+	let wrapperStyle = $derived.by(() => {
+		if (borderColors.length === 1) {
+			return `background: ${borderColors[0]}; padding: 4px; border-radius: 0.4rem;`;
+		} else {
+			return `background: linear-gradient(to bottom right, ${borderColors[0]}, ${borderColors[1]}); padding: 4px; border-radius: 0.4rem;`;
+		}
+	});
 	let open = $state(false);
 	let innerHeight = $state(0);
 	let innerWidth = $state(0);
 </script>
 
-<Card class="p-5 max-w-none relative">
-	<div class="flex gap-1 mb-3">
-		<MonSprite {mon} {PF} />
-		<div class="flex flex-col justify-between">
-			<Heading tag="h5">{mon.nickname.join('')}</Heading>
-			<div class="flex gap-3">
-				<ItemIcon heldItem={mon.heldItem} {PF} />
-				{#each form.types as type}
-					<TypeIcon {type} />
-				{/each}
+<div style={wrapperStyle}>
+	<Card class="p-6 max-w-none relative border-0">
+		<div class="flex gap-1 mb-3">
+			<MonSprite {mon} {PF} />
+			<div class="flex flex-col justify-between">
+				<Heading tag="h5">{mon.nickname.join('')}</Heading>
+				<div class="flex gap-3">
+					<ItemIcon heldItem={mon.heldItem} {PF} />
+					{#each form.types as type}
+						<TypeIcon {type} />
+					{/each}
+				</div>
 			</div>
 		</div>
-	</div>
-	{#if 'currentHP' in mon && !mon.isEgg}
-		<Healthbar {mon} />
-	{/if}
-	<P>Lv. {mon.level}</P>
-	<P>Held Item: {mon.heldItem}</P>
-	<P>Ability: {mon.ability}</P>
-	<P>Nature: {mon.nature}</P>
-	<div class="flex absolute bottom-5 right-5 gap-3">
-		{#if 'currentHP' in mon && !mon.isEgg && (mon.status.name != 'None' || mon.currentHP != mon.stats[0])}
-			<Heal bind:mon {PF} />
+		{#if 'currentHP' in mon && !mon.isEgg}
+			<Healthbar {mon} />
 		{/if}
-		<Button
-			class="p-2! border-gray-300 hover:bg-gray-300 size-11"
-			outline
-			color="dark"
-			onclick={() => (open = true)}><EditSolid class="text-gray-600 dark:text-gray-400" /></Button
-		>
-	</div>
-</Card>
-
+		<P>Lv. {mon.level}</P>
+		<P>Held Item: {mon.heldItem}</P>
+		<P>Ability: {mon.ability}</P>
+		<P>Nature: {mon.nature}</P>
+		<div class="flex absolute bottom-5 right-5 gap-3">
+			{#if 'currentHP' in mon && !mon.isEgg && (mon.status.name != 'None' || mon.currentHP != mon.stats[0])}
+				<Heal bind:mon {PF} />
+			{/if}
+			<Button
+				class="p-2! border-gray-300 hover:bg-gray-300 size-11"
+				outline
+				color="dark"
+				onclick={() => (open = true)}><EditSolid class="text-gray-600 dark:text-gray-400" /></Button
+			>
+		</div>
+	</Card>
+</div>
 <Drawer
 	bind:open
 	placement={innerWidth > innerHeight ? 'right' : 'bottom'}
