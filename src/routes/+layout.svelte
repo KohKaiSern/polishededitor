bb7d37922b8f12ef2ddc746f996e7aca67f573f0
Koh Kai Sern <kaisern.koh@gmail.com>
2025-12-31 05:25:32 +0800

    Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
