add9eb4d8e9218813e357c3cb9c254a2c5d685e8
Koh Kai Sern <kaisern.koh@gmail.com>
2026-01-21 12:44:35 +0800

    Add meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,10 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Polished Editor - Cheating/Save Editing Tool for Polished Crystal</title>
+	<meta
+		name="description"
+		content="Cheating tool for Polished Crystal that works by editing your save file via an intuitive UI. Edit Pokemon Stats, Nicknames, Bag Contents, Pokedex Data, and more!"
+	/>
 </svelte:head>
 
 {@render children()}
